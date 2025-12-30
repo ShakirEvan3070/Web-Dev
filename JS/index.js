30d1a9a359a7b83bcf7d73b85a0374d984a8a68e
unknown <shakir.evan.3070@Fec.edu.bd>
2025-12-30 17:48:49 +0600

    js

---
+++ JS/index.js
@@ -0,0 +1,48 @@
+// console.log("Hello JavaScript")
+// let x = ["Apple", "Banana", "Cherry", 1,2,3]
+// console.log(x)
+// let y = [];
+// x.push(...x);
+// console.log(y);
+// x[1]= "Orange"
+// console.log(x[0])
+// console.log(x[1])
+// console.log(x[2])
+// console.log(x[3])
+// console.log(x[4])
+// console.log(x[5])
+// let source = [1,2,3,4,5,6,7,8,9,10]
+// let target = []
+// target.push(...source)
+// console.log(source)
+// console.log(target)
+
+// let num =1
+// let sum =0
+// while (num <=10){
+//     console.log(num)
+//     console.log(sum)
+//     sum = sum + num
+//     console.log("sum:",sum)
+//     num++;
+// }
+
+let num = 1;
+while ( num <= 10){
+    console.log(num);
+    if (num % 2 === 0){
+        console.log("Even Number:", num);
+     }
+            num++; 
+}
+
+
+
+
+
+
+
+
+
+
+
