9619b1f0e0be5e1dbc92332dc9f99c7519e9e4a0
unknown <shakir.evan.3070@Fec.edu.bd>
2026-01-07 19:06:30 +0600

    min max

---
+++ JS/day2.js
@@ -0,0 +1,29 @@
+// const heights  = [167, 190, 120, 137, 200, 105];
+
+// function max (numbers){
+//     let largest = numbers[0];
+//     for (const number of numbers){
+//         if (number > largest){
+//             largest = number;
+//         }
+//     }
+//     return largest;
+// }
+// const maximum  = max(heights);
+// console.log("Maximum among these numbers are ", maximum); 
+
+
+// const weight = [45, 67, 89, 23, 20, 78];
+
+// function getMin (numbers){
+//     let smallest = weight[0];
+//     for(const number of weight){
+//         if (number <smallest){
+//             smallest = number;
+//         }
+//     }
+//     return smallest;
+// }
+
+// const min = getMin(weight);
+// console.log("Minimum number is ", min)
