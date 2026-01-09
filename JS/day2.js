8f9fd8156c3ae96f18faf537d3924872c024fd26
unknown <shakir.evan.3070@Fec.edu.bd>
2026-01-09 12:08:23 +0600

    function with loop

--- JS/day2.js
+++ JS/day2.js
@@ -27,3 +27,58 @@
 
 // const min = getMin(weight);
 // console.log("Minimum number is ", min)
+
+// const products = [
+//     {name: 'Laptop', price: 45},
+//     {name: 'Phone', price: 15},
+//     {name: 'Tablet', price: 25},
+//     {name: 'Monitor', price: 30},
+//     {name: 'Keyboard', price: 5}
+// ]
+
+// function getShoppingTotal(products){
+//     let sum = 0;
+//     for (const product of products){
+//         sum = sum + product.price;
+//         // console.log(product)
+//     }
+//     return sum;
+// }
+// const total = getShoppingTotal(products);
+// console.log("Total costing of the items are : ", total)
+
+
+// const products = [
+//     {name: 'Laptop', price: 45,quantity:2},
+//     {name: 'Phone', price: 15, quantity:3},
+//     {name: 'Tablet', price: 25, quantity:4},
+//     {name: 'Monitor', price: 30, quantity:2},
+//     {name: 'Keyboard', price: 5, quantity:5}
+// ]
+    
+// function getShoppingTotal(products){
+//     let sum = 0;
+//     for (const product of products){
+//         const price = product.price * product.quantity;
+//         sum = sum + price;
+//         // console.log(product)
+//     }
+//     return sum;
+// }
+
+// let total = getShoppingTotal(products);
+// console.log("price of the total items are : ",total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
