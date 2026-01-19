98aea4063b53b466b415c7c9f997a9b4e89d46fc
unknown <shakir.evan.3070@Fec.edu.bd>
2026-01-19 18:07:55 +0600

    AddEventListener

--- JS/EventPractice/index.js
+++ JS/EventPractice/index.js
@@ -1,15 +1,37 @@
 
-function makeYellow(){
-    console.log("Yellow")
-    document.body.style.backgroundColor = "yellow";
-}
-function makeRed(){
-    document.body.style.backgroundColor = "red";
-}
+// function makeYellow(){
+//     console.log("Yellow")
+//     document.body.style.backgroundColor = "yellow";
+// }
+// function makeRed(){
+//     document.body.style.backgroundColor = "red";
+// }
 
-function makeGreen(){
-    document.body.style.backgroundColor = "Green"
-}
-function makeBlue(){
-    document.body.style.backgroundColor= "blue"
-}
+// function makeGreen(){
+//     document.body.style.backgroundColor = "Green"
+// }
+// function makeBlue(){
+//     document.body.style.backgroundColor= "blue"
+// }
+
+document.getElementById('btn-make-green').addEventListener('click',function makeGreen(){
+    console.log("Green")
+    document.body.style.backgroundColor = "green";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
