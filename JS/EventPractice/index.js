f1b82b87fc7aef287e0674ec685a059c402bbf84
unknown <shakir.evan.3070@Fec.edu.bd>
2026-01-19 17:52:48 +0600

    event

---
+++ JS/EventPractice/index.js
@@ -0,0 +1,15 @@
+
+function makeYellow(){
+    console.log("Yellow")
+    document.body.style.backgroundColor = "yellow";
+}
+function makeRed(){
+    document.body.style.backgroundColor = "red";
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor = "Green"
+}
+function makeBlue(){
+    document.body.style.backgroundColor= "blue"
+}
